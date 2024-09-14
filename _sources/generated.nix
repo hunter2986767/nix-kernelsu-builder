86dec55866f86bd57e901f8d1c8f8ba3889acc40
hunter2986767 <hunter2986767@users.noreply.github.com>
2024-09-14 06:17:02 +0000

    auto: update packages

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -66,7 +66,7 @@
     version = "11928";
     src = fetchurl {
       url = "https://github.com/tiann/KernelSU";
-      sha256 = "sha256-ffr+rmgNP2JHs3jS54EmW3uUZxPu+FKMD1D7UW4LqeI=";
+      sha256 = "sha256-maEyYyOAcPiG1smGINoylFuwsBzqJ29Z+7BdImA1aAE=";
     };
   };
   linux-amazon-karnak = {
